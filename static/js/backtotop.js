34370441b26d03a1603aedec2fc7b2c69695d0e5
Nelson Areal <nareal@gmail.com>
2015-05-03 17:00:58 +0100

    Define variable in javascript of jQuery navigation arrow.

--- static/js/backtotop.js
+++ static/js/backtotop.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
     // browser window scroll (in pixels) after which the "back to top" link is shown
     var offset = 400,
         //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-        //offset_opacity = 700,
+        offset_opacity = 700,
         //duration of the top scrolling animation (in ms)
         scroll_top_duration = 700,
         //grab the "back to top" link
@@ -11,7 +11,7 @@ jQuery(document).ready(function($){
     //hide or show the "back to top" link
     $(window).scroll(function(){
         ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-        if( $(this).scrollTop() > offset_opacity ) { 
+        if( $(this).scrollTop() > offset_opacity ) {
             $back_to_top.addClass('cd-fade-out');
         }
     });
